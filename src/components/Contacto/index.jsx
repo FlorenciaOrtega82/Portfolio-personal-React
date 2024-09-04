2455d223ee2543ad92b82b0625b3a31cc7ff1e01
Ortega Florencia <florortega055@gmail.com>
2024-09-04 12:09:41 -0300

    Añado EmailJS, footer, formulario de contacto y cambios pequeños

---
+++ src/components/Contacto/index.jsx
@@ -0,0 +1,164 @@
+import { useState } from "react";
+import styles from "./Contacto.module.css";
+import emailjs from "emailjs-com";
+
+const Contacto = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSent, setIsSent] = useState(false);
+
+    const validate = () => {
+        let errors = {};
+
+        if (!formData.name) {
+            errors.name = "El nombre es requerido";
+        }
+
+        if (!formData.email) {
+            errors.email = "El email es requerido";
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            errors.email = "El email no es válido";
+        }
+
+        if (!formData.message) {
+            errors.message = "El mensaje es requerido";
+        }
+
+        return errors;
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const errors = validate();
+        setErrors(errors);
+
+        if (Object.keys(errors).length === 0) {
+            setIsSubmitting(true);
+            emailjs
+                .sendForm(
+                    "service_ujkp5k9",
+                    "template_kvzivjg",
+                    e.target,
+                    "Ck7NQTHS1E7wfln_J"
+                )
+                .then(
+                    (result) => {
+                        console.log(result.text);
+                        setIsSent(true);
+                        setIsSubmitting(false);
+                    },
+                    (error) => {
+                        console.log(error.text);
+                        setIsSubmitting(false);
+                    }
+                );
+
+            setFormData({
+                name: "",
+                email: "",
+                message: "",
+            });
+        }
+    };
+
+    return (
+        <>
+            <div className={styles.container__presentacion}>
+                <div className={styles.text__container}>
+                    <h2>Contacto</h2>
+                    <p>
+                        <span>¿Quieres contactarme?</span>
+                        <br />
+                        Complete el siguiente formulario y me pondré en contacto
+                        con usted lo antes posible.
+                    </p>
+                </div>
+
+                <div className={styles.form__container}>
+                    {isSent && <p>¡Mensaje enviado exitosamente!</p>}
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.entrada}>
+                            <label>Nombre:</label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={formData.name}
+                                onChange={handleChange}
+                            />
+                            {errors.name && (
+                                <p
+                                    style={{
+                                        color: "red",
+                                        fontSize: "16px",
+                                    }}
+                                >
+                                    {errors.name}
+                                </p>
+                            )}
+                        </div>
+
+                        <div className={styles.entrada}>
+                            <label>Email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                            {errors.email && (
+                                <p
+                                    style={{
+                                        color: "red",
+                                        fontSize: "16px",
+                                    }}
+                                >
+                                    {errors.email}
+                                </p>
+                            )}
+                        </div>
+
+                        <div className={styles.entrada}>
+                            <label>Mensaje:</label>
+                            <input
+                                type="text"
+                                name="message"
+                                value={formData.message}
+                                onChange={handleChange}
+                            />
+                            {errors.message && (
+                                <p style={{ color: "red", fontSize: "16px" }}>
+                                    {errors.message}
+                                </p>
+                            )}
+                        </div>
+
+                        <button
+                            type="submit"
+                            disabled={isSubmitting}
+                            className={styles.button__enviar}
+                        >
+                            {isSubmitting ? "Enviando..." : "Enviar Mensaje"}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Contacto;
--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,16 +1,18 @@
-import Presentacion from '../Presentacion'
-import Secciones from '../Secciones'
-import SobreMi from '../SobreMi'
-import styles from './Container.module.css'
+import Contacto from "../Contacto";
+import Presentacion from "../Presentacion";
+import Secciones from "../Secciones";
+import SobreMi from "../SobreMi";
+import styles from "./Container.module.css";
 
 const Container = () => {
-  return (
-  <div className={styles.container__body}>
-    <SobreMi/>
-    <Presentacion/>
-    <Secciones/>
-  </div>
-  )
-}
+    return (
+        <div className={styles.container__body}>
+            <SobreMi />
+            <Presentacion />
+            <Secciones />
+            <Contacto />
+        </div>
+    );
+};
 
-export default Container
+export default Container;
