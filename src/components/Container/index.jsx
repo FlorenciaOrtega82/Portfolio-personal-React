0c8a46cd21a887aec488b0e141c6aff597a50ec4
Ortega Florencia <florortega055@gmail.com>
2024-11-12 21:23:50 -0300

    Cambios en archivo container con tailwind

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,11 +2,11 @@ import Contacto from "../Contacto";
 import Presentacion from "../Presentacion";
 import Secciones from "../Secciones";
 import SobreMi from "../SobreMi";
-import styles from "./Container.module.css";
+
 
 const Container = () => {
     return (
-        <div className={styles.container__body}>
+        <div className='mx-60 mt-20 text-xl max-sm:mx-16 max-sm:text-sm max-md:mx-8 max-md:text-base max-lg:mx-[4.5rem] max-lg:text-lg '>
             <SobreMi />
             <Presentacion />
             <Secciones />
